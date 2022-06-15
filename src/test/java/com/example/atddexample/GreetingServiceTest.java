fb30d940f6dcc8987264c86f4bcfed5d7caeaf44
ejoongseok <ejoongseok@gmail.com>
2022-06-15 10:25:55 +0900

    Implement method in Test
    테스트에 직접 서비스 메소드를 구현한다.
    
    이때 실패하는지도 인자를 바꿔서 확인해서 우리의 테스트가 성공하는 경우와 실패하는 경우 모두를 검증하는지 확인

--- src/test/java/com/example/atddexample/GreetingServiceTest.java
+++ src/test/java/com/example/atddexample/GreetingServiceTest.java
@@ -1,14 +1,23 @@
 package com.example.atddexample;
 
-import static org.assertj.core.api.Assertions.*;
-import static org.junit.jupiter.api.Assertions.*;
 
+import static org.assertj.core.api.Assertions.*;
+import static org.mockito.BDDMockito.*;
+
+import java.util.Optional;
+
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.hamcrest.MockitoHamcrest;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
+@ExtendWith(MockitoExtension.class)
 class GreetingServiceTest {
 	private GreetingService greetingService;
+
+	@Mock EmployeeRepository repository;
 
 	@BeforeEach
 	void setUp() {
@@ -17,12 +26,29 @@ class GreetingServiceTest {
 
 	@Test
 	void greet_with_nonExisting_last_name_should_return_default_message() {
-		// given
+		// String nonExistingLastName = "nonExistingLastName";
+		// String msg = greetingService.greet(nonExistingLastName);
+		// assertThat(msg).isEqualTo("Who is this " + nonExistingLastName + " you're talking about?");
 		String nonExistingLastName = "nonExistingLastName";
-		// when
-		String msg = greetingService.greet(nonExistingLastName);
-		// then
+		String existingLastName = "existingLastName";
+		String firstName = "firstName";
+		String lastName = "lastName";
+		given(repository.findByLastName(nonExistingLastName))
+			.willReturn(Optional.empty());
+		given(repository.findByLastName(existingLastName))
+			.willReturn(Optional.of(new Employee(lastName, firstName)));
+
+		Optional<Employee> employee = repository.findByLastName(nonExistingLastName);
+		String msg = employee.map(e -> String.format("Hello %s %s!", e.getFirstName(), e.getLastName()))
+			.orElse("Who is this " + nonExistingLastName + " you're talking about?");
+
 		assertThat(msg).isEqualTo("Who is this " + nonExistingLastName + " you're talking about?");
+
+		employee = repository.findByLastName(existingLastName);
+		msg = employee.map(e -> String.format("Hello %s %s!", e.getFirstName(), e.getLastName()))
+			.orElse("Who is this " + nonExistingLastName + " you're talking about?");
+		assertThat(msg).isEqualTo("Hello " + firstName + " " + lastName + "!");
+
 	}
 
 
